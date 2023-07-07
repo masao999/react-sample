4f45fa7b75156a9b90ff9ca338fd1660da0e16ea
masao999 <42270841+masao999@users.noreply.github.com>
2023-07-08 05:22:09 +0900

    状態 (state) を持たせ、状態を更新する

--- src/FormApp.js
+++ src/FormApp.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 
 export class FormApp extends Component {
-    handleInput() {
-      console.log('handleInput called!');
+    constructor(props) {
+      super(props);
+    
+      this.state = {
+        value: '',
+        message: ''
+      };
+    }
+
+    handleInput({ target: { value } }) {
+      this.setState({
+        value
+      });
     }
   
     send() {
-      console.log('send called!');
+      const { value } = this.state;
+      this.setState({
+        value: '',
+        message: value
+      });
     }
   
     render() {
       return (
         <div>
-          <input type="text" onChange={this.handleInput.bind(this)} />
+          <input type="text" value={this.state.value} onChange={this.handleInput.bind(this)} />
           <button onClick={this.send.bind(this)}>SEND</button>
+          <div>{this.state.message}</div>
         </div>
       );
     }
   }
-  
+
