331bb46f0462238829b2c2b038df22cd1a665db5
masao999 <42270841+masao999@users.noreply.github.com>
2023-07-08 05:12:37 +0900

    フォームの見た目だけ作成する

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { FormApp } from './FormApp';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello world</h1>
+      <FormApp />
     );
   }
 }
