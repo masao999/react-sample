b6133429719c0ba30eed1fbf5609faf3ccb9bf72
masao999 <42270841+masao999@users.noreply.github.com>
2023-07-08 05:16:02 +0900

    フォームにイベント検知機能を追加する

--- src/FormApp.js
+++ src/FormApp.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 export class FormApp extends Component {
-  render() {
-    return (
-      <div>
-        <input type="text" />
-        <button>SEND</button>
-      </div>
-    );
+    handleInput() {
+      console.log('handleInput called!');
+    }
+  
+    send() {
+      console.log('send called!');
+    }
+  
+    render() {
+      return (
+        <div>
+          <input type="text" onChange={this.handleInput.bind(this)} />
+          <button onClick={this.send.bind(this)}>SEND</button>
+        </div>
+      );
+    }
   }
-}
+  
